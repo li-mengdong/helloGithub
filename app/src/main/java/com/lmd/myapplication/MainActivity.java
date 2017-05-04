57bb3aa94f6f6a7568098c951f8b7abd0cb55146
li-mengdong <lmdzyd@163.com>
2017-05-04 22:04:18 +0800

    版本控制第一版

---
+++ app/src/main/java/com/lmd/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.lmd.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //代码提交1
+
+
+        //代码提交2
+        //代码提交3
+        // 代码提交4
+    }
+}
